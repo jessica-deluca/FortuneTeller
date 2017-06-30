f8b393d470d437f2d74ca6388f80c4483ecdafbe
Your Name <you@example.com>
2017-06-30 13:04:38 -0400

    finished part 1

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -42,14 +42,19 @@ namespace FortuneTeller
             if (colorLower == "help")
             {
                 Console.WriteLine("Color options: red, orange, yellow, green, blue, indigo, violet");
+                userColor = Console.ReadLine();
             }
-            else
-            {
-
-            }
-          
 
             //Ask the user for the number of siblings the user has
+
+            Console.WriteLine("Please enter the number of siblings.");
+            int numberOfSiblings = int.Parse(Console.ReadLine());
+
+            // If the user’s age is an odd number, then they will retire in ____ years, or ___ years if their age is an even number.
+            // If the user’s number of siblings is 0, then they will have a vacation home in ____(location), or 1 then they will have a vacation home in ____(location), or 2 then they will have a vacation home in ____(location), or 3 then they will have a vacation home in ____(location), or more than 3 then they will have a vacation home in ____(location).If the user enters anything other than a number greater than or equal to 0, they should get a bad vacation home!
+            // Depending on which ROYGBIV color is the user’s favorite, they will have a specific mode of transportation(i.e.car, boat, plane, etc.)
+            // If the user’s birth month is 1 - 4, they will have $____ in the bank; if the user's birth month is 5-8, they will have $____ in the bank; and if it is 9-12, then they will have $____ in the bank. If the user enters something other than 01-12 as their birth month, they will have $0.00 in the bank.        
+
 
 
 
